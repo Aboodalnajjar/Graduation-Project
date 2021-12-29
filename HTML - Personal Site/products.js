5307941fda2e3eead0c3a4479362b5f3956623d1
Abood Alnajjar <47919203+Aboodalnajjar@users.noreply.github.com>
2021-12-29 12:38:34 +0200

    Update products.js

--- HTML - Personal Site/products.js
+++ HTML - Personal Site/products.js
@@ -37,6 +37,10 @@ function buttonAnimation(event) {
   event.preventDefault();
   switchayah(event.target.value);
 }
+const selectElement1 = document.querySelector('#ToCity');
+selectElement1.addEventListener('change', Distance);
+const selectElement2 = document.querySelector('#fromCity');
+selectElement2.addEventListener('change', Distance);
 function pricevalue(key){
   var a = document.getElementsByClassName("service-btn");
   Distance();
@@ -138,8 +142,28 @@ window.onclick = function(event) {
   }
 };
 
-function addProduct(target) {
-  // this is just a product placeholder
-  var productAdded = '<li class="cd-cart__product"><div class="cd-cart__image"><a href="#0"><img src="assets/img/product-preview.png" alt="placeholder"></a></div><div class="cd-cart__details"><h3 class="truncate"><a href="#0">Product Name</a></h3><span class="cd-cart__price">$25.99</span><div class="cd-cart__actions"><a href="#0" class="cd-cart__delete-item">Delete</a><div class="cd-cart__quantity"><label for="cd-product-' + productId + '">Qty</label><span class="cd-cart__select"><select class="reset" id="cd-product-' + productId + '" name="quantity"><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option></select><svg class="icon" viewBox="0 0 12 12"><polyline fill="none" stroke="currentColor" points="2,4 6,8 10,4 "/></svg></span></div></div></div></li>';
-  cartList.insertAdjacentHTML('beforeend', productAdded);
-}
+// Get the modal
+var modal2 = document.getElementById("pricemodal");
+
+// Get the button that opens the modal
+var btn2 = document.getElementById("Ok");
+
+// Get the <span> element that closes the modal
+var span2 = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn2.onclick = function() {
+  modal2.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span2.onclick = function(){
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+};
