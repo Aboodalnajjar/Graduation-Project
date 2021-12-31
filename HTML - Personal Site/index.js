1d9299717037d362b2066f51a5a58a6b8e86d9be
Abood Alnajjar <47919203+Aboodalnajjar@users.noreply.github.com>
2021-12-31 14:35:19 +0200

    Update index.js

--- HTML - Personal Site/index.js
+++ HTML - Personal Site/index.js
@@ -1,25 +1,40 @@
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-};
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function(){
-  modal.style.display = "none";
-};
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+var n = document.querySelectorAll(".menu-btn");
+for (var i = 0; i < n.length; i++) {
+  n[i].addEventListener("click", buttonAnimation);
+}
+function buttonAnimation(event) {
+    event.preventDefault();
+    switchayah(event.target.value);
   }
-};
+
+  function switchayah(key) {
+
+    var a = document.getElementById("user-form");
+    var b = document.getElementById("order-form");
+    switch (key) {
+      case "Account Info":
+        a.classList.add("shown");
+        a.classList.remove("hidden");
+        b.classList.add("hidden");
+        b.classList.remove("shown");
+
+        break;
+      case "My Orders":
+        a.classList.remove("shown");
+        a.classList.add("hidden");
+        a.classList.remove("shown");
+        b.classList.remove("hidden");
+        break;
+
+      default:
+      a.classList.add("hidden");
+      b.classList.add("hidden");
+    }
+  }
+
+  function doSomething(event){
+     console.log(event);
+   event.preventDefault();
+    c.classList.add("show");
+    c.classList.remove("none");
+  }
